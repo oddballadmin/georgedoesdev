be625be57d1bbf47657b6b6c672d3c887aa49cf5
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2024-03-25 22:50:07 -0400

    Finished outline for slider

--- src/components/react/Slider.tsx
+++ src/components/react/Slider.tsx
@@ -1,42 +1,75 @@
-import style from "./styles/SliderFrame.module.scss";
-import React, { useContext, useEffect } from "react";
+import style from "./styles/SliderFrame.module.css";
+import btnStyle from "./styles/Button.module.css";
+import React, { useContext, useEffect, useState } from "react";
 import { SlideValueContext } from "./context/SlideValueContext";
 import { useSliderValue } from "./hooks/UseSliderValue";
 import { SliderButton } from "./Button";
 import ReactMarkdown from "react-markdown";
 
-type SliderProps = {
-	data: Object;
+type SliderProps<T> = {
+	data: T;
 };
+interface FileData {
+	name:string,
+	content:string
 
-export const Slider = ({data}:SliderProps) => {
+}
+
+export const Slider = <T extends FileData>({ data }: SliderProps<T>) => {
+	
 	const context = useContext(SlideValueContext);
+	
 	if (context === undefined) {
 		throw new Error("useSliderValue must be used within a SliderValueProvider");
 	}
-	const { sliderValue, setSliderValue } = context;
-	useEffect(() => {
-		if (sliderValue < 0) {
-			setSliderValue(0);
-		}
-	}, [sliderValue]);
+		const { sliderValue, setSliderValue } = context;
+		const [fileArray, setFileArray] = useState<T[]>([]);
+
+		useEffect(() => {
+	        if (data) {
+	            setFileArray(Object.values(data));
+	        }
+	    }, [data]);
+		const handleLeftClick = () => {
+			setSliderValue((prevValue) => (prevValue > 0 ? prevValue - 1 : 0));
+		};
+	
+		// Function to handle right button click
+		const handleRightClick = () => {
+			setSliderValue((prevValue) => (prevValue < fileArray.length - 1 ? prevValue + 1 : fileArray.length - 1));
+		};
+
+
 	return (
-		<div className={style.sliderContainer}>
+		<div className = "grid-content relative">
 			<SliderButton
 				name="Left"
 				disabled={false}
 				position="left"
-				onClick={() => sliderValue - 1}
+				onClick={handleLeftClick}
+				className={`${btnStyle.button} ${btnStyle.buttonLeft}`}
 			>
 				Left
 			</SliderButton>
-			{JSON.stringify(data)}
+			<div className={style.sliderContainer}>
+			{
+				fileArray.map((file, index) => {
+					if (index === sliderValue) {
+						return (
+							<div key={index} className="slide-item">
+								<ReactMarkdown>{file.content}</ReactMarkdown>
+							</div>
+						);
+					}
+				})
+			}
+			</div>
 			<SliderButton
 				name="Right"
 				disabled={false}
 				position="right"
-				onClick={() => sliderValue + 1}
-			>
+				onClick={handleRightClick}
+				className={`${btnStyle.button} ${btnStyle.buttonRight}`}			>
 				Right
 			</SliderButton>
 			
